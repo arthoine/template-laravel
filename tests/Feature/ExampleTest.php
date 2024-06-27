cd14141dd137e16c9a98b7915e96d5969a4e2a02
arthoine <poeuf.alexantoine@orange.fr>
2024-06-27 22:04:44 +0200

    fix

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,3 +5,12 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use function Pest\Laravel\get;
 
 uses(RefreshDatabase::class);
+
+/**
+ * @test the app returns a successful response
+ * @return void
+ *
+ * it('the app returns a successful response', function () {
+ * get('/')->assertStatus(200);
+ * });
+ */
